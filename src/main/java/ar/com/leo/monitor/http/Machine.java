dc1eb6b30ecae71e2616fe77391c1d820f1ef81e
Esteban Vazquez <estebanvazquez1607@gmail.com>
2025-02-21 17:17:29 +0000

    started building http communication

---
+++ src/main/java/ar/com/leo/monitor/http/Machine.java
@@ -0,0 +1,14 @@
+package ar.com.leo.monitor.http;
+
+public class Machine {
+  private String code;
+
+  // getters and setters
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+}
